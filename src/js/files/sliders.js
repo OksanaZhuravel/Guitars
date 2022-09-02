47d58bf2acccae68f9962264b9081da307f51460
OksanaZhuravel <opuseva@mail.ru>
2022-09-02 21:01:18 +0400

    new block added

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -27,10 +27,86 @@ import '../../scss/base/swiper.scss';
 function initSliders() {
   // Перечень слайдеров
   // Проверяем, есть ли слайдер на стронице
-  if (document.querySelector('.swiper')) {
+  if (document.querySelector('.product__slider')) {
     // Указываем скласс нужного слайдера
     // Создаем слайдер
-    new Swiper('.swiper', {
+    new Swiper('.product__slider', {
+      // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 3,
+      spaceBetween: 0,
+      // autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      /*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+      // Пагинация
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      // Скроллбар
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+
+      // Кнопки "влево/вправо"
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      /*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+      // События
+      on: {},
+    });
+  }
+  if (document.querySelector('.promo__slider')) {
+    // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.promo__slider', {
       // Указываем скласс нужного слайдера
       // Подключаем модули слайдера
       // для конкретного случая
@@ -66,10 +142,81 @@ function initSliders() {
 
       // Скроллбар
 
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      //   draggable: true,
+      // },
+
+      // Кнопки "влево/вправо"
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      /*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+      // События
+      on: {},
+    });
+  }
+  if (document.querySelector('.reviews__slider')) {
+    // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.reviews__slider', {
+      // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      // autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      /*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+      // Пагинация
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
+
+      // Скроллбар
 
       // Кнопки "влево/вправо"
       // navigation: {
